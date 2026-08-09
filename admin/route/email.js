6babd0770c59f21ec0db64e645dc123069e7dab7
madebyzazaa <madebyzazaa@gmail.com>
2026-08-09 11:39:49 +0100

    changed logo, edited robots.txt

---
+++ admin/route/email.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const route = express.Router()
+route.use(express.json());
+route.use(express.urlencoded({extended: true }));
+const {secret} = require('../../supabase-secret');
+const { base } = require('../../supabase');
+const {Resend} = require('resend')
+const resend = new Resend(process.env.RESEND_KEY);
+const cors = require('cors')
+route.use(cors({
+    origin: 'http://127.0.0.1:5500', // Allow your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+route.get('/email/data', async(req, res)=>{
+try {
+const {data, error} = await secret.from('emails').select('*')
+if(error){console.log(error); return res.status(400)}
+if(data){return res.json({data: data})  }
+else{return res.status(400)}
+
+} catch (error) {
+console.log(error); return res.status(400)   
+}
+})
+
+//save email
+route.post('/deploy/email', async(req, res)=>{
+const email = req.body.email
+const des = req.body.des
+try {
+const {data, error} = await secret.from('emails').select().eq('email', email)
+
+if(data && data.length > 0){console.log('email exist'); return res.json({status: 'exist'})}
+
+if (error) {
+console.log('Supabase query error:', error);
+return res.status(500).json({ status: false });
+}
+const {error: err} = await secret.from('emails').insert({
+email: email,
+description: des
+})
+if(err){console.log('error saving email to supabase')
+return res.json({status: false})}
+console.log('saved email to supabase')
+return res.json({status: true})
+
+} catch (error) {
+console.log('err ', error)
+return res.json({status: false})
+}
+})
+
+//send email
+route.post('/email/send', async(req, res)=>{
+const email = req.body.email
+const subject = req.body.name
+const list = req.body.list 
+
+try {
+
+const batchPayload = list.map(userEmail => ({
+  from: 'Fresh Universe <onboard@freshuniverseworld.xyz>',
+  to: userEmail,
+  subject: subject,
+  html: email
+}));
+
+const {data, error} = await resend.batch.send(batchPayload);
+
+if(error){
+console.log('resend error', error); return res.status(400).json({status: false})
+}
+console.log('sent'); return res.status(200).json({status: true})
+} catch (error) {
+console.log(error); return res.status(500).json({status: false}) 
+}
+})
+
+module.exports = route
---
+++ public/js/drop.js
@@ -0,0 +1,61 @@
+let state = false
+function formatCountdown(targetDateString) {
+  const targetDate = new Date(targetDateString);
+  const now = new Date();
+  
+  // Calculate the difference in milliseconds
+  let difference = targetDate - now;
+
+  if (difference <= 0) {
+    document.getElementById('notify-btn').innerHTML='cop yours'
+    state=true
+    return "00:00:00:00";
+  }
+
+  // Time calculations for days, hours, and minutes
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let second = Math.floor((difference % (1000 * 60)) / 1000);
+  if(days<10){days='0'+days}
+  if(hours<10){hours='0'+hours}
+  if(minutes<10){minutes='0'+minutes}
+  if(second<10){second='0'+second}
+  // Output format: 2days: 2hrs: 30min
+  return `${days}:${hours}:${minutes}:${second}`;
+}
+
+async function load() {
+const response = await fetch('/admin/drop/data/landing')
+const data = await response.json()
+if(!data){return}
+const res = data.data[0]
+if(res.product_name===''){return}
+console.log(res)
+document.getElementById('item').innerHTML=res.details.item
+document.getElementById('graphic').innerHTML=res.details.graphic
+document.getElementById('scarcity').innerHTML=res.details.scarcity
+document.getElementById('build').innerHTML=res.details.build
+document.getElementById('fit').innerHTML=res.details.fit
+
+const timer = formatCountdown(res.timer)
+document.getElementById('timer').innerHTML=timer
+setInterval(() => {
+const timer = formatCountdown(res.timer)
+document.getElementById('timer').innerHTML=timer
+}, 1000);
+
+document.getElementById('drop-img').src=res.url
+
+}
+
+load()
+
+ document.getElementById('notify-btn').addEventListener('click', ()=>{
+  if(state){window.open('https://freshuniverseworld.myshopify.com', '_blank')}
+  else{alert('Please join the drop list on our homepage!')}
+ })
+
+ document.getElementById('footer-join').addEventListener('click', ()=>{
+alert('Please join the drop list on our homepage!')
+})
